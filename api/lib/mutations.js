48d848fb3ca3629c6c1b948f0f8012ec6bea24c5
Héctor Castillo <hectordev1999@gmail.com>
2020-02-19 17:57:05 -0600

    Create Email

--- api/lib/mutations.js
+++ api/lib/mutations.js
@@ -1,11 +1,16 @@
 'use strict'
-
+const generateName = require('./utils/Email/generateName')
 const connectDb = require('./db')
+const {ObjectID} = require('mongodb')
+
 
 module.exports = {
 
 
+  //add a new domain
   createDomain: async (root, {input}) => {
+
+    //default values
     const defaults = {
       status: false,
       created_at: new Date(),
@@ -14,35 +19,77 @@ module.exports = {
       count_emails: 0
     }
 
-
+    //apply default values
     const newDomain = Object.assign(defaults, input)
     let db
     let domain
+
+
     try {
+      //get db connection
       db = await connectDb()
+
+      //find duplicate domains
+      let duplicated = await db.collection('domain').find({
+        "host":{
+          "$eq":newDomain.host
+        }
+      }).count()
+
+      //prevent duplicate domains
+      if ( duplicated ){
+        console.error('Duplicated domain')
+        return null;
+      }
+
+      //add new domain
       domain = await db.collection('domain').insertOne(newDomain)
-      newDomain._id = course.insertedId
+      newDomain._id = domain.insertedId//add the generated id
     } catch (e) {
       console.error(e)
     }
 
+    //return a OK domain added
     return newDomain
   },
 
-  // createEmail: async (root, {input}) => {
-  //   let db
-  //   let student
-  //   try {
-  //     db = await connectDb()
-  //     student = await db.collection('email').insertOne(newCourse)
-  //     input._id = student.insertedId
-  //   } catch (e) {
-  //     console.error(e)
-  //   }
+ 
 
-  //   return input
-  // }
+    //add a new Email
+    createEmail: async (root, {domain_id}) => {
 
+      //Email
+      const Email = {
+        email: null,
+        created_at: new Date(),
+        remove_at: new Date(),
+        mails: []
+      }
+
+  
+      let db  
+      try {
+        //get db connection
+        db = await connectDb()
+  
+        //get the domain
+        let domain = await db.collection('domain').findOne({"_id":ObjectID(domain_id)})
+
+        Email.email = generateName(domain.host)
+  
+
+  
+        //add new Email
+        let newEmail = await db.collection('email').insertOne(Email)
+        Email._id = newEmail.insertedId//add the generated id
+      } catch (e) {
+        console.error(e)
+      }
+  
+      //return a OK Email added
+      return Email
+    },
+  
 
 
  }
